170515f7d01d50c5c00645edd2d2dc34899cf6c3
Paulo Prescendo <paulovinicius1994@hotmail.com>
2019-10-02 18:21:18 -0300

    frontend day 02 finished

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
+const routes = require('./routes');
 const app = express();
 
 mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0-epyzo.mongodb.net/semana09?retryWrites=true&w=majority', {
@@ -9,7 +11,9 @@ mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0-epyzo.mongodb.net/semana0
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
