b591197eb09f5412ef7076cfa98ba34c7e543d65
Paulo Prescendo <paulovinicius1994@hotmail.com>
2019-10-04 21:51:04 -0300

    advanced day 04 finished

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,30 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0-epyzo.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+const connectedUsers = {};
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  return next();
 });
 
 app.use(cors());
@@ -16,4 +34,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
